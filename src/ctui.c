a1933f0475f472fc56a3ea616fd5ef82031bb8cd
kimpors <mikita_korshak@ukr.net>
2025-06-20 14:40:35 +0300

    Separate library and executable in cmake file
    Signed-off-by: kimpors <mikita_korshak@ukr.net>

---
+++ src/ctui.c
@@ -0,0 +1,23 @@
+#include "cmdio.h"
+#include "ctui.h"
+
+void ci_winshow(CI_Window *win)
+{
+	char *s = "help there on the way to you";
+
+	for (size_t y = 0; y < win->s.row; y++)
+	{
+		comove(win->p.y + y, win->p.x);
+		for (size_t x = 0; x < win->s.col; x++)
+		{
+			if ((y == 0 && x == 0) || (y == 0 && x == win->s.col - 1)) putchar('+');
+			else if ((y == win->s.row - 1 && x == 0) || (y == win->s.row - 1 && x == win->s.col - 1)) putchar('+');
+			else if ((y == 0 || y == win->s.row - 1) && x > 0 && x < win->s.col - 1) putchar('-');
+			else if ((y > 0 && y < win->s.row - 1) && (x == 0 || x == win->s.col - 1)) putchar('|');
+			else if (*s && (y > 1 && y < win->s.row - 2) && (x > 1 && x < win->s.col - 2)) putchar(*s++);
+			else putchar(' ');
+		}
+
+		putchar('\n');
+	}
+}
